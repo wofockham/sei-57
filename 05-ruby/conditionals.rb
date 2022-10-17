f11e65a8021eed74f34335a232fb3e0549ddff3f
wofockham <certainthings@gmail.com>
2022-10-17 12:04:11 +1100

    conditional examples

---
+++ 05-ruby/conditionals.rb
@@ -0,0 +1,50 @@
+# Ruby has a rich framework for asking questions (conditionals)
+
+n = 1001
+
+if n > 5
+    puts "Wow"
+else
+    puts "Nah"
+end
+
+# Multiple possibilities:
+
+grade = 'B'
+
+if grade == 'A'
+    puts "You are killing it"
+elsif grade == 'B'
+    puts "You are coasting fine"
+elsif grade == 'C'
+    puts "Adequate"
+else # default
+    puts "Please see Loden after class"
+end
+
+# TODO: research Ruby's case statement
+
+# Ruby's bonus conditionals
+
+# Short AKA modifier AKA backwards conditionals
+n = 15
+puts "Wow" if n > 5
+
+# unless: opposite of if
+
+today_is_wednesday = false
+
+# if today_is_wednesday == false
+#     puts "Today is not Wednesday"
+# end
+
+# if !today_is_wednesday
+#     puts "Today is not Wednesday"
+# end
+
+unless today_is_wednesday
+    puts "Today is not Wednesday"
+end
+
+# unless also supports the modifier form
+puts "Today is not Wednesday" unless today_is_wednesday
