ceeb5f722ce1fa3c9a116c40ba6f9a6f260d13be
wofockham <certainthings@gmail.com>
2022-10-20 15:21:46 +1100

    butterflies CRUD all done

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -7,29 +7,54 @@ get '/' do
 end
 
 # INDEX
+# Show all the butterflies
 get '/butterflies' do
     @butterflies = query_db 'SELECT * FROM butterflies'
     erb :butterflies_index
 end
 
 # NEW
+# Blank form for a new butterfly
 get '/butterflies/new' do
     erb :butterflies_new
 end
 
 # CREATE
+# Insert the user's form data into the database
 post '/butterflies' do
     query_db "INSERT INTO butterflies (name, family, image) VALUES ('#{ params[:name] }', '#{params[:family] }', '#{ params[:image] }')"
     redirect to('/butterflies')
 end
 
 # SHOW
+# Show a single butterfly
 get '/butterflies/:id' do
     butterflies = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
     @butterfly = butterflies.first # Extract the butterfly from the array of results
     erb :butterflies_show
 end
 
+# EDIT
+# Pre-filled form for updating a butterfly
+get '/butterflies/:id/edit' do
+    butterflies = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
+    @butterfly = butterflies.first # Extract the butterfly from the array of results
+    erb :butterflies_edit
+end
+
+# UPDATE
+# Update a butterfly with the user's form data
+post '/butterflies/:id' do
+    query_db "UPDATE butterflies SET name='#{ params[:name] }', family='#{ params[:family] }', image='#{ params[:image] }' WHERE id=#{ params[:id] }"
+    redirect to("/butterflies/#{ params[:id] }")
+end
+
+# DESTROY
+# Delete a butterfly from the database
+get '/butterflies/:id/delete' do
+    query_db "DELETE FROM butterflies WHERE id=#{ params[:id] }"
+    redirect to('/butterflies')
+end
 
 def query_db(sql_statement)
     puts sql_statement # Optional but nice to have for debugging
