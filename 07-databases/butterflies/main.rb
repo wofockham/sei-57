45393d6f12973fce5912b9e20cb807616926fb7d
wofockham <certainthings@gmail.com>
2022-10-20 12:59:27 +1100

    butterflies CRUD index and show

---
+++ 07-databases/butterflies/main.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+
+get '/' do
+    erb :home
+end
+
+# INDEX
+get '/butterflies' do
+    db = SQLite3::Database.new 'database.sqlite3'
+    db.results_as_hash = true
+    @butterflies = db.execute 'SELECT * FROM butterflies'
+    db.close
+
+    erb :butterflies_index
+end
+
+# SHOW
+get '/butterflies/:id' do
+    db = SQLite3::Database.new 'database.sqlite3'
+    db.results_as_hash = true
+    butterflies = db.execute "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
+    @butterfly = butterflies.first # Extract the butterfly from the array of results
+    db.close
+    erb :butterflies_show
+end
