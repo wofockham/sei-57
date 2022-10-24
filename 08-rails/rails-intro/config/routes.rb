bc83e1902943d61a77349c42a7f6657960b9f6c7
wofockham <certainthings@gmail.com>
2022-10-24 14:13:55 +1100

    children smoking

--- 08-rails/rails-intro/config/routes.rb
+++ 08-rails/rails-intro/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root :to => 'pages#goodbye'
+
   get '/hello' => 'pages#hello' # 'controller#action'
   get '/goodbye' => 'pages#goodbye'
+  get '/funny' => 'pages#funny'
 
   get '/auto/:color' => 'auto#color'
   get '/auto/:hp/:torque' => 'auto#engine'
