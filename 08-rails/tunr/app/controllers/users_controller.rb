1a927cad3e0c8936160c976468bb7fe54f47e130
wofockham <certainthings@gmail.com>
2022-10-27 15:47:15 +1100

    user sign up and sign in

--- 08-rails/tunr/app/controllers/users_controller.rb
+++ 08-rails/tunr/app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      session[:user_id] = @user.id # log in the new user
+      redirect_to root_path # IRL: choose where to redirect
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
