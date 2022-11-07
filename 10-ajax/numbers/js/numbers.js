5b18e04d07b78425117ec305281160c154eaf256
wofockham <certainthings@gmail.com>
2022-11-07 14:46:15 +1100

    numberwang

--- 10-ajax/numbers/js/numbers.js
+++ 10-ajax/numbers/js/numbers.js
@@ -1,20 +1,15 @@
 const fetchFact = function () {
     const xhr = new XMLHttpRequest();
 
-    // xhr.readyState equals 4 MEANS that the AJAX request is finished
     xhr.onreadystatechange = function () {
         if (xhr.readyState !== 4) {
             return;
         }
 
-        // convert the JSON string into an actual Javascript object
         const data = JSON.parse( xhr.responseText );
 
-        // create a DOM node
         const p = document.createElement('p');
-        // put the text in the DOM node
         p.innerHTML = data.text;
-        // attach the DOM node to the page
         document.body.appendChild(p);
     };
 
