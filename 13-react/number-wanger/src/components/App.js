85335e3230fa067bf7fc8a359c568592345af9e0
wofockham <certainthings@gmail.com>
2022-11-14 15:11:38 +1100

    <NumberWang />

---
+++ 13-react/number-wanger/src/components/App.js
@@ -0,0 +1,12 @@
+import NumberWang from './NumberWang';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Number Facts</h1>
+      <NumberWang />
+    </div>
+  );
+}
+
+export default App;
---
+++ 13-react/number-wanger/src/components/NumberWang.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class NumberWang extends Component {
+    constructor() {
+        super();
+        this.state = {
+            facts: []
+        };
+        this._fetchFact = this._fetchFact.bind(this);
+    }
+
+    _fetchFact() {
+        axios('http://numbersapi.com/random/trivia?json').then((response) => {
+            this.setState({facts: [...this.state.facts, response.data.text]});
+
+            console.log("That's number wang!".split('').map(
+                (c) => Math.random() > 0.5 ? c.toUpperCase() : c
+            ).join(''));
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={ this._fetchFact } title={ (777 * Math.random()).toFixed(3) }>
+                    Number wang
+                </button>
+                <ul>
+                    { this.state.facts.map((fact, i) => <li key={i}>{fact}</li>) }
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default NumberWang;
