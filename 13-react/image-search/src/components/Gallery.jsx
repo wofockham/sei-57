0e452bbcb7e988e2f6ad4e9fd9ad60b257594a85
wofockham <certainthings@gmail.com>
2022-11-15 11:05:59 +1100

    comments are now more accurate

--- 13-react/image-search/src/components/Gallery.jsx
+++ 13-react/image-search/src/components/Gallery.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
-// Functional components are quick and easy to use
-// if you don't need state.
-
-// They behave just like the render() method.
+// Functional components behave just like the render() method.
 const Gallery = function (props) {
     console.log(props);
     return (
