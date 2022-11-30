3d6551cceb53a699a80db886a82d71601d3dc20a
wofockham <certainthings@gmail.com>
2022-11-30 15:07:23 +1100

    more vue component examples

---
+++ 14-advanced/vue-fundamentals/src/components/NumberFacts.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <button @click="fetchFact">Fetch fact</button>
+        <button @click="clearFacts">Clear</button>
+        <p v-for="(fact, i) in facts" :key="i">{{ fact }}</p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'NumberFacts',
+    data: function () {
+        return {
+            facts: []
+        };
+    },
+    methods: {
+        fetchFact: function () {
+            axios.get('http://numbersapi.com/random/trivia').then((response) => {
+                this.facts.push(response.data);
+            });
+        },
+        clearFacts: function () {
+            this.facts = [];
+        }
+    }
+}
+</script>
--- 14-advanced/vue-fundamentals/src/views/ExperimentsView.vue
+++ 14-advanced/vue-fundamentals/src/views/ExperimentsView.vue
@@ -4,17 +4,36 @@
 
         <ClickrButton />
 
-        <button>dumb old button</button>
+        <hr>
+
+        <SocialProfile name="Groucho" bio="No gods no masters" pic="http://www.placekitten.com/400/600" />
+        <SocialProfile name="Harpo" pic="http://www.placekitten.com/400/601" />
+        <SocialProfile name="Chico" bio="That's a'nice" pic="http://www.placekitten.com/401/600" />
+
+        <hr>
+
+        <FancyCalculator />
+
+        <hr>
+
+        <NumberFacts />
+
     </div>
 </template>
 
 <script>
 // @ is a shortcut for `src`
 import ClickrButton from '@/components/ClickrButton.vue';
+import SocialProfile from '@/components/SocialProfile.vue';
+import FancyCalculator from '@/components/FancyCalculator.vue';
+import NumberFacts from '@/components/NumberFacts.vue';
 
 export default {
     components: {
-        ClickrButton: ClickrButton
+        ClickrButton: ClickrButton,
+        SocialProfile: SocialProfile,
+        FancyCalculator: FancyCalculator,
+        NumberFacts: NumberFacts
     }
 };
 
