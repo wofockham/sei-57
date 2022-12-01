fe7ec7666a9035344388fc7a29018ee59d4e9d11
wofockham <certainthings@gmail.com>
2022-12-01 15:35:56 +1100

    complete express + mongodb CRUD JSON api

--- 14-advanced/vocab-server/server.js
+++ 14-advanced/vocab-server/server.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+global.Vocab = require('./api/models/vocabModel');
+const routes = require('./api/routes/vocabRoutes');
+
+mongoose.Promise = global.Promise;
+mongoose.connect( `mongodb+srv://theBlade:${ process.env.MONGOPW }@cluster0.i0cbj.mongodb.net/?retryWrites=true&w=majority` )
+
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+routes(app); // attach our routes to the server
 app.listen(port);
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
+// a 404 "page not found"
+app.use((req, res) => {
+    res.status(404).send({ url: `${ req.originalUrl } not found`});
 });
 
 console.log(`Server started on port: http://localhost:${ port }`);
