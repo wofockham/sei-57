f3df6f2f9e88ef464d28ac4c88fe29d3247144a8
wofockham <certainthings@gmail.com>
2022-12-05 16:30:11 +1100

    Enumerable mixin

--- 12-comp-sci/linked-lists/singly_linked_list.rb
+++ 12-comp-sci/linked-lists/singly_linked_list.rb
@@ -1,4 +1,6 @@
 class SinglyLinkedList
+    include Enumerable # mixin: simulates inheriting from multiple classes
+
     class Node
         attr_accessor :value, :next
     
@@ -37,6 +39,11 @@ class SinglyLinkedList
 
     # TODO: More methods
     def remove # AKA .shift -- remove the first node
+        if @head
+            previous_head = @head
+            @head = previous_head.next
+            previous_head.value
+        end
     end
 
     def insert_after(node, new_value)
@@ -47,14 +54,27 @@ class SinglyLinkedList
 
     # Tricky
     def reverse # non-destructive
+        reversed_list = SinglyLinkedList.new
+        current_node = @head
+        while current_node
+            reversed_list.prepend current_node.value
+            current_node = current_node.next
+        end
+        reversed_list
     end
 
     # Trickier
     def reverse!
+        @head = self.reverse.head
     end
 
     # Trickiest
     def each # how do you execute a block in Ruby?
+        current_node = @head
+        while current_node
+            yield current_node.value if block_given? # execute the do/end block
+            current_node = current_node.next
+        end
     end
 
     # Bonus: .map(), .reduce(), .select(), .reject(), .length() AKA .size() AKA count()
@@ -63,6 +83,8 @@ end
 
 
 bros = SinglyLinkedList.new 'Groucho'
+bros.append 'Harpo'
+bros.append 'Chico'
 
 require 'pry'
 binding.pry
